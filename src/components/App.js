adc0621b8fa9840d98f018750f67cb2161264796
kohichi <kohichi.horii@gmail.com>
2019-05-03 14:44:56 -0700

    Add static button so user can log in or skip to main page

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 import Error404 from './Error404';
 import Header from './Header';
-import Home from './Home';
+import Welcome from './Welcome';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Header/>
       <Switch>
-        <Route exact path='/' component={Home} />
+        <Route exact path='/' component={Welcome} />
         <Route component={Error404}/>
       </Switch>
     </div>
---
+++ src/components/Welcome.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components'
+
+function Welcome(){
+  return(
+    <div>
+      <h1>Year of fun</h1>
+      <p>Relive the moments or make new ones</p>
+      <button>log in</button>
+      <br />
+      <button>skip to main page</button>
+    </div>
+  )
+}
+
+export default Welcome
