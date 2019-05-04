a6789811e3de2c022e51f6914283b9e4a14bd19a
kohichi <kohichi.horii@gmail.com>
2019-05-03 17:08:13 -0700

    Add static artist list and also links so user can sign in

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -3,8 +3,14 @@ import styled from 'styled-components'
 
 function EventList() {
   return(
-    <div>
-
+    <div className="nav-links">
+      <li>Drake</li>
+      <li>Ed Sheeran</li>
+      <li>The Weekend</li>
+      <li>Maroon</li>
+      <li>Kanye West</li>
+      <a href="#">Log In</a>
+      <a href="#">Share</a>
     </div>
   )
 }
--- src/components/Music.js
+++ src/components/Music.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import styled from 'styled-components'
+import EventList from './EventList'
 
 function Music(){
   return(
     <div>
-      <h1>Music</h1>
-      <h3>Major Lazer</h3>
-      <button>learn more</button>
+      <div className="mainPage">
+        <h1>Music</h1>
+        <h3>Major Lazer</h3>
+        <button>learn more</button>
+      </div>
+      <EventList />
     </div>
   )
 }
