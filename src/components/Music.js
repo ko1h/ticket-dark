aafd8d135e393fafb53de4148cd9c9022ee06f71
kohichi <kohichi.horii@gmail.com>
2019-05-10 11:45:17 -0700

    Add option to buy tickets in music  component

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 import EventList from './EventList'
 
-function Music(){
+function Music(props){
   return(
     <div>
       <div className="mainPage">
-        <h1>Music</h1>
-        <h3>Major Lazer</h3>
-        <button>learn more</button>
+        <img src={toji} />
+        <h1>{props.name}</h1>
+        <h3>{props.brand}</h3>
+        <p>{props.details}</p>
       </div>
-      <EventList />
+      <button onClick={handleToBuyTicket}>BUy</button>
+      </div>
     </div>
   )
 }
