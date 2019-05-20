e1ca1cbddfdeafa389c91266aeae1348d8661080
kohichi <kohichi.horii@gmail.com>
2019-05-19 17:25:14 -0700

    fixed issue with project not compiling, had scripts file into src

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { createStore, applyMiddleware} from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './reducers';
-import * as serviceWorker from './serviceWorker';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import './index.scss';
+import App from './components/App';
+import { AppContainer } from 'react-hot-loader';
+import { HashRouter } from 'react-router-dom';
+//import * as serviceWorker from './serviceWorker';
 
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <HashRouter>
+        <Component />
+      </HashRouter>
+    </AppContainer>,
+    document.getElementById('root')
+  );
+}
 
-const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+render(App);
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
- document.getElementById('root')
-);
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App);
+  });
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
