be4fa10ab20ede970b35960517dad4183eb708f4
kohichi <kohichi.horii@gmail.com>
2019-05-20 18:15:23 -0700

    fixing issue with search bar positioning

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import styled from 'styled-components'
 import wallpaper from '../assets/wallpaper.png'
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
 
-function Welcome(){
+
+
+function Welcome(props){
+
   return(
     <div className="Welcome">
+      <style jsx>{`
+    .search.top {
+      display: flex;
+      position: absolute;
+    }
+    `}</style>
       <img src={wallpaper}/>
-      <div>
-        <input type="Search" placeholder="Search"/>
+      <div className='search top'>
+        <SearchIcon />
+        <InputBase
+          placeholder="Search…"
+          />
       </div>
     </div>
   )
